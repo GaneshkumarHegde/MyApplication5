dfb65fb7d830a6e0787f2d24c64a0d4e10487043
GaneshkumarHegde <ganeshirekai@gmail.com>
2018-05-31 20:30:28 +0530

    Continue With Google

--- app/src/main/java/com/example/windows/myapplication/MainActivity.java
+++ app/src/main/java/com/example/windows/myapplication/MainActivity.java
@@ -1,22 +1,83 @@
 package com.example.windows.myapplication;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
     }
 
 
-    public void press(View view) {
-        Intent i;
-        i = new Intent(MainActivity.this,Main2Activity.class);
-        startActivity(i);
+    public void login(View view)
+    {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragSpace,new LoginActivity()).commit();
     }
+
+    public void createAccount(View view)
+    {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragSpace,new CreateAccount()).commit();
+    }
+
+    public void continueWithGoogle(View view)
+    {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,0);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if(account!=null){
+            Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+            startActivity(intent);
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 0) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task)
+    {
+        if(task!=null){
+            Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+            startActivity(intent);
+        }
+    }
+
 }
